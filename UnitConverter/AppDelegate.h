d70b7e4879266d822402c54981dbca124c50bf02
Benedicte Karlsen <benedictek1@gmail.com>
2016-07-13 15:19:08 +0200

    Initial Commit

---
+++ UnitConverter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UnitConverter
+//
+//  Created by Benedicte Karlsen on 13.07.2016.
+//  Copyright © 2016 Hello World Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UnitConverter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UnitConverter
+//
+//  Created by Benedicte Karlsen on 13.07.2016.
+//  Copyright © 2016 Hello World Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
